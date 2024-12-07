ce6d1a9f511c4df8f34eb0383118157b9e7567ce
nasim0994 <nasimuddin0994@gmail.com>
2024-12-07 19:28:35 +0600

    update notice

--- frontEnd/src/Layout/MainRootLayout.jsx
+++ frontEnd/src/Layout/MainRootLayout.jsx
@@ -1,13 +1,13 @@
 import { Outlet } from "react-router-dom";
 import CircularProgress from "../Components/ProgressScroll/ProgressScroll";
-// import AdminNotice from "../Components/AdminNotice/AdminNotice";
+import AdminNotice from "../Components/AdminNotice/AdminNotice";
 
 export default function MainRootLayout() {
   return (
     <>
       <Outlet />
 
-      {/* <AdminNotice /> */}
+      <AdminNotice />
       <CircularProgress />
     </>
   );
